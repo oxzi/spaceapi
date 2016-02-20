3f4560854748685f79e4d61e3a2922875571ca01
geistesk <github.jyf300hj@0x21.biz>
2016-02-20 02:07:39 +0100

    Fixed typo in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SpaceApi.Mixfile do
     [app: :spaceapi,
      version: "0.1.0",
      elixir: "~> 1.2",
-     description: "A small Elixir package for parsing the Space API"
+     description: "A small Elixir package for parsing the Space API",
      package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
