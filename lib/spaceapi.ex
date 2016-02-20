8c43a9a8bf050fa16212acb247e56dd146b1ab68
geistesk <github.jyf300hj@0x21.biz>
2016-02-20 01:47:58 +0100

    Documentation for SpaceApi

--- lib/spaceapi.ex
+++ lib/spaceapi.ex
@@ -1,10 +1,17 @@
 defmodule SpaceApi do
   alias SpaceApi.Space
   import SpaceApi.JsonParser, only: [parse_json: 2]
+  @moduledoc """
+  This module provides access to the _from_string_-function.
+  """
 
+  @doc """
+  A given string (should contain a valid Space API-JSON) will be converted
+  into a Space-structure.
+  """
+  @spec from_string(str: String, space: Space) :: Space
   def from_string(str, space \\ %Space{}) do
     Poison.decode!(str)
     |> parse_json(space)
   end
-
 end
